80a67ecd4bfe2c28b41fcb95d356a05bc2831bf5
Youri <youribok@msn.com>
2018-08-14 01:10:07 +0200

    Fixes day1 part2 line endings (start)

--- src/2017/day1/part2.js
+++
@@ -1,8 +0,0 @@
-const process = require('./process');
-
-module.exports = (input) => {
-    const digits = input.split('').map(Number);
-
-    // Filter all input digits to select only those identical to the opposite digit, which is halfway around the list
-    return process(digits, index => digits[(index + digits.length / 2) % digits.length]);
-};
