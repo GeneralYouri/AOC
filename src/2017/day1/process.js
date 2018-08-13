5172724269760a40613fc257a51e4d9143748046
Youri <youribok@msn.com>
2018-08-14 01:12:02 +0200

    Fixes day1 process line endings (start)

--- src/2017/day1/process.js
+++
@@ -1,5 +0,0 @@
-module.exports = (digits, getNextDigit) => {
-    return digits
-        .filter((digit, index) => digit === getNextDigit(index))
-        .reduce((sum, digit) => sum + digit, 0);
-};
