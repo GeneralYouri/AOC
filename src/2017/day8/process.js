12b2ad449fe8441290c4f4d4d489c3a00161fa8a
Youri <youribok@msn.com>
2018-08-27 20:53:55 +0200

    Refactors day8 parts 1&2 to better divide responsibilities across the various functions being used

--- src/2017/day8/process.js
+++ src/2017/day8/process.js
@@ -1,43 +1,42 @@
-const registers = {};
-
 const parseLine = /^(\w+)\s+(inc|dec)\s+(-?\d+)\s+if\s+(\w+)\s+([=!<>]+)\s+(-?\d+)$/;
 
 const conditions = {
-    '==': (register, value) => registers[register] === value,
-    '!=': (register, value) => registers[register] !== value,
-    '<=': (register, value) => registers[register] <= value,
-    '>=': (register, value) => registers[register] >= value,
-    '<': (register, value) => registers[register] < value,
-    '>': (register, value) => registers[register] > value,
+    '==': (value, compare) => value === compare,
+    '!=': (value, compare) => value !== compare,
+    '<=': (value, compare) => value <= compare,
+    '>=': (value, compare) => value >= compare,
+    '<': (value, compare) => value < compare,
+    '>': (value, compare) => value > compare,
 };
 
 const operations = {
-    inc: (register, amount) => {
-        registers[register] += amount;
-    },
-    dec: (register, amount) => {
-        registers[register] -= amount;
-    },
+    inc: (value, amount) => value + amount,
+    dec: (value, amount) => value - amount,
 };
 
-function checkCondition(register, operation, value) {
+const getRegister = (registers, register) => {
     if (!(register in registers)) {
         registers[register] = 0;
     }
 
-    return conditions[operation](register, Number(value));
-}
+    return registers[register];
+};
 
 module.exports = (commands, onAfterOperation = () => {}) => {
+    const registers = {};
+
     commands.forEach((commandString) => {
         const [register, operation, amount, ...condition] = parseLine.exec(commandString).slice(1, 7);
+        const [conditionRegister, conditionOperation, conditionCompare] = condition;
 
-        if (!(register in registers)) {
-            registers[register] = 0;
-        }
+        const value = getRegister(registers, register);
+        const conditionValue = getRegister(registers, conditionRegister);
 
-        if (checkCondition(...condition)) {
-            operations[operation](register, Number(amount));
+        const applyCondition = conditions[conditionOperation];
+        const applyOperation = operations[operation];
+
+        if (applyCondition(conditionValue, Number(conditionCompare))) {
+            registers[register] = applyOperation(value, Number(amount));
         }
 
         onAfterOperation(registers[register]);
