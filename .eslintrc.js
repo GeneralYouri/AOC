db3b34b0d6e8c6d636ea8a97540aeb3429881849
Youri <youribok@msn.com>
2018-09-11 04:19:18 +0200

    Improves clarity on ESLint config by replacing numeric rule settings with string ones

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,11 @@
 module.exports = {
+    root: true,
     extends: 'airbnb-base',
     plugins: ['jest'],
     env: {
         es6: true,
-        node: true,
         jest: true,
+        node: true,
     },
     globals: {
         // Temporary workaround as long as ESLint can't properly support BigInt
@@ -17,34 +18,34 @@ module.exports = {
     },
     rules: {
         // Allow debugger during development
-        'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         // Allow console during development
-        'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
 
         // Other custom rules
-        'max-len': 0,
+        'max-len': 'off',
         indent: ['error', 4, { SwitchCase: 1 }],
 
         // This rule sounds good in theory, but there are too many exceptions where reassignment is wanted/needed, and the whitelist doesn't suffice
         'no-param-reassign': 'off',
 
         // Operator precedence is a thing for a reason; there are plenty of cases where forcing extra brackets does not lead to an improvement
-        'no-mixed-operators': 0,
+        'no-mixed-operators': 'off',
 
         // Simple function bodies can sometimes still be complex enough to benefit of the extra readability provided by a simple newline
-        'arrow-body-style': 0,
+        'arrow-body-style': 'off',
 
         // In some cases, regular string concatenation still looks and feels superior
-        'prefer-template': 0,
+        'prefer-template': 'off',
 
         // This seems like a fairly stupid rule to enable; it disables simple anonymous functions and arrow functions, even when only used as callback
-        'no-loop-func': 0,
+        'no-loop-func': 'off',
 
         // Even AirBnB themselves allow this, just use with caution
-        'no-bitwise': 0,
+        'no-bitwise': 'off',
 
         /** @see https://github.com/airbnb/javascript#modules--no-webpack-loader-syntax */
-        'import/no-webpack-loader-syntax': 2,
+        'import/no-webpack-loader-syntax': 'error',
 
         // Specifying unused function arguments isn't a problem and can add clarity
         /** @see https://blog.javascripting.com/2015/09/07/fine-tuning-airbnbs-eslint-config/ */
@@ -58,10 +59,10 @@ module.exports = {
         'no-else-return': 'off',
 
         // For this Node application, for .. of loops are just fine
-        'no-restricted-syntax': 0,
+        'no-restricted-syntax': 'off',
 
         // Used 'after' since before airbnb-base enforced this rule with 'before', no need to change now
-        'operator-linebreak': [2, 'after'],
+        'operator-linebreak': ['error', 'after'],
 
         // Another rule not specified by AirBnB themselves; continues can be useful to prevent excessive tabbing and separate logic
         'no-continue': 'off',
