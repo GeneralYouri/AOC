cd89012672033d809816c2d175f5076b88de3f12
Youri <youribok@msn.com>
2018-10-03 17:46:34 +0200

    Adds missing test input for day 19

--- src/2017/day19/part1.test.js
+++ src/2017/day19/part1.test.js
@@ -1,7 +1,14 @@
 const { part1, defaultInput } = require('./');
 
 test('Provided test cases', () => {
-    expect(part1()).toBe('ABCDEF');
+    expect(part1('' +
+        '     |          \n' +
+        '     |  +--+    \n' +
+        '     A  |  C    \n' +
+        ' F---|----E|--+ \n' +
+        '     |  |  |  D \n' +
+        '     +B-+  +--+ \n' +
+        '                ')).toBe('ABCDEF');
 });
 
 test('Puzzle input', () => {
--- src/2017/day19/part2.test.js
+++ src/2017/day19/part2.test.js
@@ -1,7 +1,14 @@
 const { part2, defaultInput } = require('./');
 
 test('Provided test cases', () => {
-    expect(part2()).toBe(38);
+    expect(part2('' +
+        '     |          \n' +
+        '     |  +--+    \n' +
+        '     A  |  C    \n' +
+        ' F---|--|-E|--+ \n' +
+        '     |  |  |  D \n' +
+        '     +B-+  +--+ \n' +
+        '                ')).toBe(38);
 });
 
 test('Puzzle input', () => {
