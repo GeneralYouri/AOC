e84ac846204e2654da7aba5493bbc5511b8a6b01
GeneralYouri <youribok@msn.com>
2018-12-05 06:40:39 +0100

    Adds 2018 day 5 parts 1 & 2 solutions

---
+++ src/2018/day5/part2.test.js
@@ -0,0 +1,13 @@
+const { part2, defaultInput } = require('./');
+
+test('Provided test cases', () => {
+    expect(part2('')).toBe();
+    expect(part2('')).toBe();
+    expect(part2('')).toBe();
+    expect(part2('')).toBe();
+    expect(part2('')).toBe();
+});
+
+test('Puzzle input', () => {
+    expect(part2(defaultInput)).toBe();
+});
