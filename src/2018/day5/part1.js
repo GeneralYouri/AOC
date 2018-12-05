ea0bf8aa07aa86fb42d846518b15038bf8231944
GeneralYouri <youribok@msn.com>
2018-12-05 06:41:08 +0100

    Refactors 2018 day 5 parts 1 & 2 solutions

--- src/2018/day5/part1.js
+++ src/2018/day5/part1.js
@@ -1,35 +1,16 @@
-const isPolarOpposite = (charCode1, charCode2) => Math.abs(charCode1 - charCode2) === 32;
-
-const fn = (charCodes, [exclude1, exclude2]) => {
-    let lastLength = charCodes.length;
+module.exports = (polymer) => {
+    let lastLength = polymer.length;
 
     while (true) {
-        for (let i = 0; i < charCodes.length - 1; i += 1) {
-            const charCode1 = charCodes[i];
-            const charCode2 = charCodes[i + 1];
-            if (charCode1 === exclude1 || charCode1 === exclude2) {
-                continue;
-            }
-            if (isPolarOpposite(charCode1, charCode2)) {
-                charCodes.splice(i, 2);
-                // charCodes = [...charCodes.slice(0, i), charCodes.slice(i + 2)];
+        for (let i = 0; i < polymer.length - 1; i += 1) {
+            if (Math.abs(polymer.charCodeAt(i) - polymer.charCodeAt(i + 1)) === 32) {
+                polymer = polymer.substring(0, i) + polymer.substring(i + 2);
             }
         }
 
-        if (charCodes.length === lastLength) {
-            return charCodes.length;
+        if (polymer.length === lastLength) {
+            return polymer.length;
         }
-        lastLength = charCodes.length;
+        lastLength = polymer.length;
     }
-};
-
-module.exports = (input) => {
-    const charCodes = input.split('').map(char => char.charCodeAt(0));
-    return Array.from(new Array(26)).map((_, i) => i + 65).reduce((acc, exclude1) => {
-        const exclude2 = exclude1 + 32;
-        const subInput = charCodes.filter(charCode => charCode !== exclude1 && charCode !== exclude2);
-        console.log(input.length, subInput.length);
-        const result = fn(subInput, [exclude1, exclude2]);
-        return Math.min(acc, result);
-    }, Number.POSITIVE_INFINITY);
 };
--- src/2018/day5/part2.js
+++ src/2018/day5/part2.js
@@ -1,3 +1,9 @@
+const doReactions = require('./part1');
+
 module.exports = (input) => {
-    return undefined;
+    return Array.from(new Array(26)).map((_, i) => [String.fromCharCode(i + 65), String.fromCharCode(i + 97)]).reduce((acc, [exclude1, exclude2]) => {
+        const subInput = input.replace(new RegExp('[' + exclude1 + exclude2 + ']', 'g'), '');
+        const result = doReactions(subInput, [exclude1, exclude2]);
+        return Math.min(acc, result);
+    }, Number.POSITIVE_INFINITY);
 };
