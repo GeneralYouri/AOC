3439cb0c1055f51b53a9f194dc98db16e2e43c84
GeneralYouri <youribok@msn.com>
2018-12-06 21:18:26 +0100

    Adds tests for 2018 day 6 parts 1 & 2 solutions

--- src/2018/day6/part1.test.js
+++ src/2018/day6/part1.test.js
@@ -1,13 +1,16 @@
 const { part1, defaultInput } = require('./');
 
 test('Provided test cases', () => {
-    expect(part1('')).toBe();
-    expect(part1('')).toBe();
-    expect(part1('')).toBe();
-    expect(part1('')).toBe();
-    expect(part1('')).toBe();
+    const input =
+        '1, 1\n' +
+        '1, 6\n' +
+        '8, 3\n' +
+        '3, 4\n' +
+        '5, 5\n' +
+        '8, 9';
+    expect(part1(input)).toBe(17);
 });
 
 test('Puzzle input', () => {
-    expect(part1(defaultInput)).toBe();
+    expect(part1(defaultInput)).toBe(4754);
 });
--- src/2018/day6/part2.test.js
+++ src/2018/day6/part2.test.js
@@ -1,13 +1,16 @@
 const { part2, defaultInput } = require('./');
 
 test('Provided test cases', () => {
-    expect(part2('')).toBe();
-    expect(part2('')).toBe();
-    expect(part2('')).toBe();
-    expect(part2('')).toBe();
-    expect(part2('')).toBe();
+    const input =
+        '1, 1\n' +
+        '1, 6\n' +
+        '8, 3\n' +
+        '3, 4\n' +
+        '5, 5\n' +
+        '8, 9';
+    expect(part2(input, 32)).toBe(16);
 });
 
 test('Puzzle input', () => {
-    expect(part2(defaultInput)).toBe();
+    expect(part2(defaultInput)).toBe(42344);
 });
