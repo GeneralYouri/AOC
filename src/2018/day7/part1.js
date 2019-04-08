2b393e58b42a23ae3bc2448d879f8ea62410699c
GeneralYouri <youribok@msn.com>
2019-04-08 02:24:51 +0200

    Refactors 2018 day 7 parts 1 & 2 solutions

--- src/2018/day7/part1.js
+++ src/2018/day7/part1.js
@@ -1,35 +1,22 @@
 module.exports = (input, stepCount = 26) => {
-    const rules = input.split(/\n/g).map(line => [line[5], line[36]]);
+    const rules = input.split(/\n/g).map(line => [5, 36].map(index => line.charCodeAt(index) - 65));
 
     const stepNames = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').slice(0, stepCount);
-    const allowed = new Set();
-    const required = stepNames.map((name) => {
-        const list = rules.reduce((acc, [from, to]) => {
-            if (to === name) {
-                acc.add(from);
-            }
-            return acc;
-        }, new Set());
-        if (list.size === 0) {
-            allowed.add(name);
-        }
-        return list;
+    const required = stepNames.map(() => 0);
+    rules.forEach(([fromIndex, toIndex]) => {
+        required[toIndex] |= 1 << fromIndex;
     });
 
     let order = '';
+    let used = 0;
     while (order.length < stepCount) {
-        const toAdd = stepNames.find(name => allowed.has(name));
-        order += toAdd;
-        allowed.delete(toAdd);
+        const nextIndex = stepNames.findIndex((name, index) => (used & (1 << index)) === 0 && required[index] === 0);
+        order += stepNames[nextIndex];
+        used |= 1 << nextIndex;
 
-        required.forEach((list, index) => {
-            list.delete(toAdd);
-            const name = String.fromCharCode(index + 65);
-            if (!order.includes(name) && list.size === 0) {
-                allowed.add(name);
-            }
+        required.forEach((mask, index) => {
+            required[index] &= ~(1 << nextIndex);
         });
     }
-
     return order;
 };
--- src/2018/day7/part2.js
+++ src/2018/day7/part2.js
@@ -1,57 +1,41 @@
 module.exports = (input, stepCount = 26, workerCount = 5, flatDelayPerStep = 60) => {
-    const rules = input.split(/\n/g).map(line => [line[5], line[36]]);
+    const rules = input.split(/\n/g).map(line => [5, 36].map(index => line.charCodeAt(index) - 65));
 
     const stepNames = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').slice(0, stepCount);
-    const allowed = new Set();
-    const required = stepNames.map((name) => {
-        const list = rules.reduce((acc, [from, to]) => {
-            if (to === name) {
-                acc.add(from);
-            }
-            return acc;
-        }, new Set());
-        if (list.size === 0) {
-            allowed.add(name);
-        }
-        return list;
+    const required = stepNames.map(() => 0);
+    rules.forEach(([fromIndex, toIndex]) => {
+        required[toIndex] |= 1 << fromIndex;
     });
     const workers = Array.from(new Array(workerCount)).fill(0);
-    const workingOn = Array.from(new Array(workerCount)).fill('');
+    const workingOn = Array.from(new Array(workerCount)).fill(-1);
 
-    let order = '';
     let time = 0;
+    let order = '';
+    let used = 0;
     while (order.length < stepCount) {
         for (let w = 0; w < workerCount; w += 1) {
-            if (workers[w] === 1) {
-                const toAdd = workingOn[w];
-                order += toAdd;
-                workingOn[w] = '';
+            workers[w] -= 1;
+            if (workers[w] === 0) {
+                const nextIndex = workingOn[w];
+                order += stepNames[nextIndex];
+                workingOn[w] = -1;
 
-                required.forEach((list, index) => {
-                    list.delete(toAdd);
-                    const name = String.fromCharCode(index + 65);
-                    if (!order.includes(name) && list.size === 0 && !allowed.has(name) && !workingOn.includes(name)) {
-                        allowed.add(name);
-                    }
+                required.forEach((mask, index) => {
+                    required[index] &= ~(1 << nextIndex);
                 });
             }
         }
         for (let w = 0; w < workerCount; w += 1) {
-            if (workers[w] > 0) {
-                workers[w] -= 1;
-            }
-            if (workers[w] === 0) {
-                const toAdd = stepNames.find(name => allowed.has(name));
-                if (toAdd) {
-                    allowed.delete(toAdd);
-                    workers[w] = toAdd.charCodeAt(0) - 64 + flatDelayPerStep;
-                    workingOn[w] = toAdd;
+            if (workers[w] <= 0) {
+                const nextIndex = stepNames.findIndex((name, index) => (used & (1 << index)) === 0 && required[index] === 0);
+                if (nextIndex !== -1) {
+                    workers[w] = nextIndex + flatDelayPerStep + 1;
+                    workingOn[w] = nextIndex;
+                    used |= 1 << nextIndex;
                 }
             }
         }
-
         time += 1;
     }
-
     return time - 1;
 };
