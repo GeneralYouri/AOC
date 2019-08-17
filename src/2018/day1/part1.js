2824d19d727cb939d59557b9e1336d0cca1a4f75
GeneralYouri <youribok@msn.com>
2019-08-17 18:17:11 +0200

    Refactors 2018 day 1 part 2 solution

--- src/2018/day1/part1.js
+++ src/2018/day1/part1.js
@@ -1,5 +1,5 @@
-module.exports = input => input.split(/\n/g)
-    .map(Number)
-    .reduce((frequency, change) => frequency + change, 0);
+// module.exports = input => input.split(/\n/g)
+//     .map(Number)
+//     .reduce((frequency, change) => frequency + change, 0);
 
 module.exports = eval;
--- src/2018/day1/part2.js
+++ src/2018/day1/part2.js
@@ -1,27 +1,39 @@
-module.exports = (input) => {
-    const deltas = input.split(/\n/g).map(Number);
-    const seen = new Set();
-    let frequency = 0;
-
-    while (true) {
-        for (const delta of deltas) {
-            seen.add(frequency);
-            frequency += delta;
-            if (seen.has(frequency)) {
-                return frequency;
-            }
-        }
-    }
-};
+// module.exports = (input) => {
+//     const deltas = input.split(/\n/g).map(Number);
+//     const seen = new Set();
+//     let frequency = 0;
+//
+//     while (true) {
+//         for (const delta of deltas) {
+//             seen.add(frequency);
+//             frequency += delta;
+//             if (seen.has(frequency)) {
+//                 return frequency;
+//             }
+//         }
+//     }
+// };
 
 module.exports = (input) => {
     const deltas = input.split(/\n/g).map(Number);
     const seen = {};
     let frequency = 0;
 
+    for (let i = 0; i < deltas.length; i += 1) {
+        seen[frequency] = true;
+        frequency += deltas[i];
+        if (seen[frequency]) {
+            return frequency;
+        }
+    }
+
+    const cycleDelta = frequency;
+    if (cycleDelta === 0) {
+        return 0;
+    }
+
     while (true) {
         for (let i = 0; i < deltas.length; i += 1) {
-            seen[frequency] = true;
             frequency += deltas[i];
             if (seen[frequency]) {
                 return frequency;
