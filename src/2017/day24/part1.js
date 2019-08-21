d5ad70ced912b28734c2a9d5ef7f7e60b6f17c9b
GeneralYouri <youribok@msn.com>
2019-08-21 20:39:58 +0200

    Refactors 2018 day 24 parts 1 & 2 solutions

--- src/2017/day24/part1.js
+++ src/2017/day24/part1.js
@@ -1,14 +1,47 @@
-const recurse = require('./recurse');
+// const process = require('./process');
+//
+// module.exports = (input) => {
+//     let bestWeight = 0;
+//     const processBridge = (weight, length) => {
+//         if (weight > bestWeight) {
+//             bestWeight = weight;
+//         }
+//     };
+//
+//     process(input, processBridge);
+//     return bestWeight;
+// };
 
 module.exports = (input) => {
-    const components = input.split(/\n/g).map(line => line.split(/\//g).map(Number).sort((a, b) => a - b)).sort((a, b) => a[0] - b[0] || a[1] - b[1]);
+    const components = input.split(/\n/g).map(line => line.split(/\//g).map(Number));
+    const componentsByPort = Array.from(Array(51)).map(() => []);
 
-    const bridges = [];
-    components.forEach(([port1, port2], index) => {
-        if (port1 === 0) {
-            recurse(bridges, port1 + port2, 1, port2, components.filter((_, i) => i !== index));
+    for (let i = 0; i < components.length; i += 1) {
+        const component = components[i];
+        const portA = component[0];
+        const portB = component[1];
+        const c = [portA + portB, false];
+        componentsByPort[portA].push(c);
+        if (portA !== portB) {
+            componentsByPort[portB].push(c);
         }
-    });
+    }
 
-    return bridges.reduce((acc, [weight]) => Math.max(acc, weight), 0);
+    let bestWeight = 0;
+    const recurse = (weight, length, endPort) => {
+        if (weight > bestWeight) {
+            bestWeight = weight;
+        }
+
+        for (let i = 0; i < componentsByPort[endPort].length; i += 1) {
+            const component = componentsByPort[endPort][i];
+            if (!component[1]) {
+                component[1] = true;
+                recurse(weight + component[0], length + 1, component[0] - endPort);
+                component[1] = false;
+            }
+        }
+    };
+    recurse(0, 0, 0);
+    return bestWeight;
 };
--- src/2017/day24/process.js
+++ src/2017/day24/process.js
@@ -1,22 +1,4 @@
-// const process = require('./process');
-//
-// module.exports = (input) => {
-//     let bestLength = 0;
-//     let bestWeight = 0;
-//     const processBridge = (weight, length) => {
-//         if (length > bestLength) {
-//             bestLength = length;
-//             bestWeight = weight;
-//         } else if (length === bestLength && weight > bestWeight) {
-//             bestWeight = weight;
-//         }
-//     };
-//
-//     process(input, processBridge);
-//     return bestWeight;
-// };
-
-module.exports = (input) => {
+const process = (input, cb) => {
     const components = input.split(/\n/g).map(line => line.split(/\//g).map(Number));
     const componentsByPort = Array.from(Array(51)).map(() => []);
 
@@ -31,16 +13,8 @@ module.exports = (input) => {
         }
     }
 
-    let bestLength = 0;
-    let bestWeight = 0;
     const recurse = (weight, length, endPort) => {
-        if (length > bestLength) {
-            bestLength = length;
-            bestWeight = weight;
-        } else if (length === bestLength && weight > bestWeight) {
-            bestWeight = weight;
-        }
-
+        cb(weight, length);
         for (let i = 0; i < componentsByPort[endPort].length; i += 1) {
             const component = componentsByPort[endPort][i];
             if (!component[1]) {
@@ -51,5 +25,6 @@ module.exports = (input) => {
         }
     };
     recurse(0, 0, 0);
-    return bestWeight;
 };
+
+module.exports = process;
