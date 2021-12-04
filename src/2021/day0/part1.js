0846d0a17f0e84ba7eef2ad78aef4679ec9b6c0d
Youri Bok <GeneralYouri@users.noreply.github.com>
2021-12-04 09:45:38 +0100

    Updates 2021 day template to incorporate some helpers

--- src/2021/day0/part1.js
+++ src/2021/day0/part1.js
@@ -1,9 +1,8 @@
+require('../library.js');
+
 module.exports = (input) => {
-    const lines = input.split(/\n/g).map(line => line.match(/(.*)/).slice(1));
+    const lines = input.lines().map(line => line.match(/(.*)/).slice(1));
     for (const line of lines) {
         console.log(line);
     }
-
-    const N = input.split(/,/g).map(Number);
-    return N.reduce((sum, n) => sum + n, 0);
 };
--- src/2021/day0/part2.js
+++ src/2021/day0/part2.js
@@ -1,9 +1,8 @@
-module.exports = (input) => {
-    const lines = input.split(/\n/g).map(line => line.match(/(.*)/).slice(1));
-    for (const line of lines) {
-        console.log(line);
-    }
+require('../library.js');
 
-    const N = input.split(/,/g).map(Number);
-    return N.reduce((sum, n) => sum + n, 0);
+module.exports = (input) => {
+    const lines = input.lines().map(line => line.match(/(.*)/).slice(1));
+    for (const line of lines) {
+        // console.log(line);
+    }
 };
